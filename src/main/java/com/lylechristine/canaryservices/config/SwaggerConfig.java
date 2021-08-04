b57c185d674d8b58eaf2adb61361c0f3aae919b9
Lylio <cxz@gmx.com>
2021-08-04 21:13:25 +0100

    swagger config aadded

--- src/main/java/com/lylechristine/canaryservices/config/SwaggerConfig.java
+++ src/main/java/com/lylechristine/canaryservices/config/SwaggerConfig.java
@@ -2,11 +2,21 @@ package com.lylechristine.canaryservices.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RequestMethod;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.builders.ResponseMessageBuilder;
+import springfox.documentation.schema.ModelRef;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.service.ResponseMessage;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
@@ -16,6 +26,21 @@ public class SwaggerConfig {
                 .select()
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
-                .build();
+                .build()
+                .apiInfo(getApiInformation());
     }
+
+    private ApiInfo getApiInformation(){
+        return new ApiInfo("Canary Services -  REST API",
+                "Canary Services is the poor bird that lets us devs know if it's save to proceed further into" +
+                        " the darkness of the codebase.",
+                "1.0",
+                "API Terms of Service URL",
+                new Contact("Lyle Christine", "https://github.com/lylio", "nope@nope.com"),
+                "API License",
+                "API License URL",
+                Collections.emptyList()
+        );
+    }
+
 }
