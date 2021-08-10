ab4c686d36effcf05967b54ed6161031014df9fb
Lylio <cxz@gmx.com>
2021-08-10 22:36:01 +0100

    logging level messages updated

--- src/main/java/com/lylechristine/canaryservices/controllers/LoggingController.java
+++ src/main/java/com/lylechristine/canaryservices/controllers/LoggingController.java
@@ -16,11 +16,11 @@ public class LoggingController {
 
     @RequestMapping("/api/v2/logger")
     public String logIndex() {
-        logger.trace("A trace message");
-        logger.debug("A debug message");
-        logger.info("An info message");
-        logger.warn("A warn message");
-        logger.error("An error message");
+        logger.trace("A trace message: Tracing the path of the app, like a rubber duck floating down a river");
+        logger.debug("A debug message: Lots and lots and lots of info here");
+        logger.info("An info message: Some info, for your convenience");
+        logger.warn("A warn message: Serious stuff to be checked on");
+        logger.error("An error message: DANGER!!!!");
 
         return ("<h2>Howdy! Check out the logs to see the logging messages!<h2>" +
                 "<br> <h2>Logging level in application.properties = " + logLevel + "<h2>");
